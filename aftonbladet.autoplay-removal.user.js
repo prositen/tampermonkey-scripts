775cd4de0303a3ec9570bbdd79dd55a7bfb846b9
Anna Holmgren <prositen@gmail.com>
2016-11-18 15:39:47 +0100

    Create aftonbladet.autoplay-removal.user.js
    Added script for Aftonbladet

---
+++ aftonbladet.autoplay-removal.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         Aftonbladet autoplay removal
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Remove autoplay from videos
+// @author       prositen
+// @match        *://www.aftonbladet.se/*
+// @grant        none
+// @require      https://code.jquery.com/jquery-3.1.1.min.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var jq = jQuery.noConflict();
+    jq('iframe[allowfullscreen=true]').each(function(i,v) {
+        var src = v.src;
+        var new_src = src.replace('autoplay=true', 'autoplay=false');
+        if (new_src == src) {
+            new_src += "&autoplay=false";
+        }
+        v.src = new_src;
+    });
+})();
