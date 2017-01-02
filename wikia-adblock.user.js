4d10bd01ebef79ac40fdd27229e174b3ed48ec29
Anna Holmgren <prositen@gmail.com>
2017-01-02 14:12:05 +0100

    Create wikia-adblock.user.js

---
+++ wikia-adblock.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Remove Wikia adblock warnings
+// @namespace    https://github.com/prositen
+// @version      0.1
+// @description  Remove adblock warnings from wikia
+// @author       prositen
+// @match        *://*.wikia.com/*
+// @require      https://code.jquery.com/jquery-3.1.1.min.js
+// @grant        none
+// @run-at       document-body
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    ads.context.opts = {};
+})();
