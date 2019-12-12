b03edcc340757baefe000fec7c6793e77ed48496
Anna Holmgren <prositen@prositen.com>
2019-12-12 13:33:19 +0100

    Create twitter.remove-sidebar.js

---
+++ twitter.remove-sidebar.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Remove twitter sidebar
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Remove unwanted stuff (promoted, other people's likes, ...)
+// @author       Anna Holmgren
+// @match        *://*.twitter.com/*
+// @require      https://code.jquery.com/jquery-3.4.1.slim.min.js
+// @update       https://github.com/prositen/tampermonkey-scripts/raw/master/twitter.remove-sidebar.js
+// @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @grant        none
+// ==/UserScript==
+
+waitForKeyElements("[data-testid='sidebarColumn']", hideIt);
+
+function hideIt(node) {
+   node.hide();
+}
